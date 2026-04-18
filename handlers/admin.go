c0ebea83661cf3f345f0a1b62f8cdb0dd32eb698
Данж Опасность <dangeroday21@gmail.com>
2026-04-19 03:31:11 +0500

    fix

--- handlers/admin.go
+++ handlers/admin.go
@@ -116,6 +116,8 @@ func (ah *AdminHandler) HandleAdminMenu(c telebot.Context) error {
 	)
 
 	log.Printf("✅ [ADMIN] Показываю панель настроек\n")
+	log.Printf("DEBUG HandleAdminMenu: text = %q\n", text)
+	log.Printf("DEBUG HandleAdminMenu: text bytes = %v\n", []byte(text))
 	return c.Send(text, menu)
 }
 
